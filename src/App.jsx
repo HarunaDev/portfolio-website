db66f7b44142b1acda3c8df2318aa76447024b1f
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-09 20:24:01 +0100

    modified main & App files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css'
+import NavBar from './components/NavBar'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+      <NavBar />
+
     </>
   )
 }
