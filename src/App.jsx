137cc587dc81c083f55832c713553dd260cb4222
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-09 20:41:53 +0100

    created routes for web page

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import './App.css'
 import NavBar from './components/NavBar'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Works from './pages/Works'
 
 function App() {
   
 
   return (
     <>
+      <NavBar />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/works' element={<Works />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
-      <NavBar />
-
     </>
   )
 }
