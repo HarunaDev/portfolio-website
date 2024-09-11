c670e8929c68581e8f3ef58e7cf4102995659b58
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 09:34:01 +0100

    styled part of the services component and rendered it in the about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,14 @@
 import Banner from "../components/Banner"
 import CtaButton from "../components/CtaButton"
+import Services from "../components/Services"
 
 function About() {
     return (
-        <section>
+        <main>
             <Banner header={<h1 className="text-light text-8xl font-bold w-[900px]">I can deliver results that <span className="text-primary">exceeds</span> your expectations.</h1>} header2={<CtaButton styles="border-4 border-primary bg-primary px-8 py-4 mt-16 text-2xl font-bold text-light rounded" url="/contact" text="Hire me"></CtaButton>}/>
-        </section>
+
+            <Services />
+        </main>
     )
 }
 
