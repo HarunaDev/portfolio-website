8025d3f715f551bc99c7c8e33fb8d91a18c2ada0
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 09:06:34 +0100

    rendered banner component but with data for about view

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,10 @@
+import Banner from "../components/Banner"
+import CtaButton from "../components/CtaButton"
+
 function About() {
     return (
         <section>
-            <h1>About Page</h1>
+            <Banner header={<h1 className="text-light text-8xl font-bold w-[900px]">I can deliver results that <span className="text-primary">exceeds</span> your expectations.</h1>} header2={<CtaButton styles="border-4 border-primary bg-primary px-8 py-4 mt-16 text-2xl font-bold text-light rounded" url="/contact" text="Hire me"></CtaButton>}/>
         </section>
     )
 }
