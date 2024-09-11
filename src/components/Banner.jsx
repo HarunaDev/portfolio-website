240aca64ed99c181119df5c6fe1429097dce4cdf
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 06:49:28 +0100

    added banner elements and style classes
    modified background color value

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,14 @@
+import Img from "../assets/banner.png"
+
 function Banner() {
     return (
-        <section>
-            <h1>this is the banner</h1>
+        <section className="h-[600px] px-48 bg-background flex justify-center items-center">
+            <div>
+                <h1 className="font-bold text-9xl mb-8 text-light">Software <span className="text-primary">Developer</span></h1>
+                <h2 className="font-semibold text-light text-3xl">My name is Alvin and I am passionate about crafting efficient and innovative solutions.</h2>
+            </div>
+            
+            <img src={Img} alt="Banner Img" />
         </section>
     )
 }
