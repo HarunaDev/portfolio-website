df48277e657b5c63a1fd562fef17265a47ccb6c0
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 18:50:32 +0100

    created slider component

---
+++ src/components/Slider.jsx
@@ -0,0 +1,9 @@
+function Slider() {
+    return (
+        <section className="border-4 h-1/2 bg-background">
+            <h1 className="text-light text-center text-4xl mt-12">This is the slider</h1>
+        </section>
+    )
+}
+
+export default Slider
