a455cdd4ba45bfa9a37181a93dea1aef4f38edca
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 09:34:01 +0100

    created component for services section

---
+++ src/components/Services.jsx
@@ -0,0 +1,11 @@
+function Services() {
+    return (
+        <section className="bg-background h-[700px] py-16 border-2">
+            <h6 className="text-center text-primary font-bold text-2xl mb-12">Services</h6>
+
+            <h2 className="text-center text-4xl text-light font-semibold w-[555px] mx-auto">Crafting custom solutions for your unique challenges.</h2>
+        </section>
+    )
+}
+
+export default Services
