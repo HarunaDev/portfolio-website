ce78c7718189a17ebd6b94ff30246b48d8271a4e
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 05:26:27 +0100

    added comments

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,18 +5,23 @@ import { Dialog, DialogPanel, Disclosure, Popover, PopoverGroup } from '@headles
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import ResumeButton from './ResumeButton'
 
-
 export default function NavBar() {
+  // navbar state
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
+    // header
     <header className="bg-background">
       <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+
+        {/* logo */}
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
             <img alt="" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" className="h-8 w-auto" />
           </Link>
         </div>
+
+        {/* nav bars */}
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -24,9 +29,11 @@ export default function NavBar() {
             className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
           >
             <span className="sr-only">Open main menu</span>
-            <Bars3Icon aria-hidden="true" className="h-6 w-6" />
+            <Bars3Icon aria-hidden="true" className="h-6 w-6 text-light" />
           </button>
         </div>
+
+        {/* nav links */}
         <PopoverGroup className="hidden lg:flex lg:gap-x-12">
           <Popover className="relative">
             <Link to="/about" className="flex items-center gap-x-1 text-lg font-semibold leading-6 text-light">
@@ -41,17 +48,26 @@ export default function NavBar() {
             Contact
           </Link>
         </PopoverGroup>
+
+        {/* resume button */}
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           {/* <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
             Resume
           </a> */}
           <ResumeButton />
         </div>
+
       </nav>
+
+      {/* nav mobile view */}
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
         <div className="fixed inset-0 z-10" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+
+        {/* mobile nav menu */}
+        <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-background px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
+
+            {/* mobile logo */}
             <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Haruna Dev</span>
               <img
@@ -60,43 +76,45 @@ export default function NavBar() {
                 className="h-8 w-auto"
               />
             </Link>
+
+            {/* mobile close icon */}
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
             >
               <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="h-6 w-6" />
+              <XMarkIcon aria-hidden="true" className="h-6 w-6 text-light" />
             </button>
+
           </div>
+
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
+            {/* mobile nav links */}
               <div className="space-y-2 py-6">
                 <Disclosure as="div" className="-mx-3">
-                  <Link to="/about" className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                  <Link to="/about" className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-light hover:text-secondary">
                     About
                   </Link>
                 </Disclosure>
                 <Link
                   to="/works"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 w-[100px] block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-light hover:text-secondary"
                 >
                   Works
                 </Link>
                 <Link
                   to="/contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-light hover:text-secondary"
                 >
                   Contact
                 </Link>
               </div>
+
+              {/* mobile resume button */}
               <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                >
-                  Resume
-                </a>
+                <ResumeButton />
               </div>
             </div>
           </div>
