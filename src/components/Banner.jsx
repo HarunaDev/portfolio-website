353fd5a95d50eab43f4cc6badf66002e48bb74c5
Alvin Haruna <harunaalvin69@gmail.com>
2024-09-11 08:47:18 +0100

    made banner component dynamic and modified the render in home page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,14 +1,12 @@
-import Img from "../assets/banner.png"
-
-function Banner() {
+function Banner(prop) {
     return (
         <section className="h-[650px] px-48 bg-background flex justify-between items-center">
             <div>
-                <h1 className="font-bold text-9xl mb-8 text-light">Software <span className="text-primary">Developer</span></h1>
-                <h2 className="font-semibold text-light text-3xl">My name is Alvin and I am passionate about crafting efficient and innovative solutions.</h2>
+                {prop.header}
+                {prop.header2}
             </div>
             
-            <img className="ml-[50px]" src={Img} alt="Banner Img" />
+            {prop.img}
         </section>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import Banner from "../components/Banner"
+import Img from "../assets/banner.png"
 
 function Home() {
     return (
         <main>
-            <Banner />
+            <Banner header={<h1 className="font-bold text-9xl mb-8 text-light">Software <span className="text-primary">Developer</span></h1>} header2={<h2 className="font-semibold text-light text-3xl">My name is Alvin and I am passionate about crafting efficient and innovative solutions.</h2>} img={<img className="ml-[50px]" src={Img} alt="Banner Img" />} />
         </main>
     )
 }
