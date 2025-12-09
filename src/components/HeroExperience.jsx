242b44e10759467c7d52b3dca95b6e45e8673a6a
Alvin Haruna <harunaalvin69@gmail.com>
2025-12-09 15:30:56 +0100

    modified chair model position on different screen sizes

--- src/components/HeroExperience.jsx
+++ src/components/HeroExperience.jsx
@@ -12,6 +12,14 @@ const HeroExperience = () => {
     const isTablet = useMediaQuery({ query: '(max-width: 1024px)'})
     const isMobile = useMediaQuery({ query: '(max-width: 760px)'})
 
+    const chairRef = useRef()
+
+    // Responsive positions
+    const chairPosition = isMobile 
+        ? [0, 7.2, -1] 
+        : isTablet 
+            ? [0, 2.2, -1.5] 
+            : [0, 4, -2] 
 
   return (
     <Canvas camera={{position: [0, 0, 15], fov: 45}} style={{ width: '100%', height: '500px' }}>
@@ -26,7 +34,7 @@ const HeroExperience = () => {
 
         <group scale={isMobile ? 0.7 : 1} position={[0, -3.5, 0]} rotation={[0, -Math.PI /3, 0]} >
         
-        <GamingChair scale={[.2, .2, .2]} position={[0, 4, -2]} rotation={[0, Math.PI, 0]}/>
+        <GamingChair scale={[.2, .2, .2]} position={chairPosition} rotation={[0, Math.PI, 0]} ref={chairRef}/>
         {/* <GamingChair scale={[0.2, 0.2, 0.2]} position={[0, 4, -2]}/> */}
 
         </group>
