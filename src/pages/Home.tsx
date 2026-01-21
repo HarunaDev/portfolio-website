63b4697088b9e31d048ea9ff9d305616f51883e6
Alvin Haruna <harunaalvin69@gmail.com>
2026-01-21 14:18:09 +0100

    Modified Home pages as tsx file

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,14 @@
-import { motion } from "framer-motion";
+import { motion, type Variants } from "framer-motion";
 import Banner from "../components/Banner";
 // import Img from "../assets/banner.png";
 
-const pageVariants = {
+const pageVariants : Variants = {
   initial: { opacity: 0, y: -50 },
   animate: { opacity: 1, y: 0 },
   exit: { opacity: 0, y: 50 },
 };
 
-function Home() {
+const Home = () : JSX.Element => {
   return (
     <motion.main
       initial="initial"
